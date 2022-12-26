64315c0a2e6fcf6064f9de6761dd6995a0cbc7a4
Saabitra Bera <63924604+Saabitra@users.noreply.github.com>
2022-12-26 20:24:10 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,2 +1,41 @@
-import bot from './assets/bot.svg';
-import user from './assets/user.svg';
+import bot from 'client/assets/bot.svg';
+import user from 'client/assets/user.svg';
+
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat-container');
+
+let loadInterval;
+
+function loader(element) {
+  element.textContent = '';
+
+  loadInterval = setInterval(() => {
+    element.textContent += '.';
+
+    if (element.textContent.length === '....') {
+      element.textContent = '';
+    }
+  }, 300)
+}
+
+function typeText(element, text) {
+  let index = 0;
+
+  let interval = setInterval(() => {
+    if (index < text.length) {
+      element.innerHTML += text.charAt(index);
+      index++;
+    } else {
+      clearInterval(interval);
+    }
+  }, 20)
+}
+
+function generateUniqueID() {
+  const timestamp = Date.now();
+  return Math.floor(Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return 'id-${timestamp}-${hexadecimalString}';
+}
